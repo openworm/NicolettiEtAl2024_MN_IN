6c539eea6e0fef2ea4a198c9b1765a7cb75a3b55
pgleeson <p.gleeson@gmail.com>
2025-03-11 16:25:17 +0000

    Add initial script for generating IV curves

--- NeuroML/clean.sh
+++ NeuroML/clean.sh
@@ -1,2 +1,2 @@
 rm -rf __pycache__ arm64
-mv *dat *txt LEMS_Test*.xml *.mod *nrn.py *.hoc /tmp
+mv *dat *txt LEMS_Test*.xml *.mod *nrn.py *.hoc LEMS_iv_*.xml iv_*.net.nml /tmp
---
+++ NeuroML/generate_iv_curve.py
@@ -0,0 +1,27 @@
+import sys
+from pyneuroml.analysis import generate_current_vs_frequency_curve
+
+from matplotlib import pyplot as plt
+
+nogui = "-nogui" in sys.argv  # Used to supress GUI in tests for Travis-CI
+
+for cell in ['AVAL', 'AVAR']:
+    generate_current_vs_frequency_curve(
+        "%s.cell.nml"%cell,
+        cell,
+        start_amp_nA=-0.03,
+        end_amp_nA=0.04,
+        step_nA=0.01,
+        analysis_duration=1000,
+        analysis_delay=100,
+        pre_zero_pulse=100,
+        post_zero_pulse=500,
+        plot_voltage_traces=not nogui,
+        plot_if=not nogui,
+        plot_iv=not nogui,
+        save_if_data_to="iv_%s.dat"%cell,
+        show_plot_already=False,
+    )
+
+if not nogui:
+    plt.show()
